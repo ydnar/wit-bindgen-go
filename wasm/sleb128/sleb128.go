36af53ee5d8caf9f209922d6863b2a5e461bf018
Randy Reddig <randy.reddig@fastly.com>
2024-11-13 10:04:13 -0800

    wasm: initial commit of SLEB128 encoding

---
+++ wasm/sleb128/sleb128.go
@@ -0,0 +1,32 @@
+// Package sleb128 implements signed LEB128 integer reads and writes.
+// Based on code from the Go standard library.
+//
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package sleb128
+
+import "io"
+
+// Write writes v in signed [LEB128] format to w.
+// Returns the number of bytes written and/or an error.
+//
+// LEB128: https://en.wikipedia.org/wiki/LEB128
+func Write(w io.ByteWriter, v int64) (n int, err error) {
+	more := true
+	for more {
+		c := uint8(v & 0x7f)
+		s := uint8(v & 0x40)
+		v >>= 7
+		more = !((v == 0 && s == 0) || (v == -1 && s != 0))
+		if more {
+			c |= 0x80
+		}
+		err = w.WriteByte(c)
+		if err != nil {
+			return n, err
+		}
+		n++
+	}
+	return n, nil
+}
---
+++ wasm/uleb128/uleb128.go
@@ -0,0 +1,31 @@
+// Package uleb128 implements unsigned LEB128 integer reads and writes.
+// Based on code from the Go standard library.
+//
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+package uleb128
+
+import "io"
+
+// Write writes v in unsigned [LEB128] format to w.
+// Returns the number of bytes written and/or an error.
+//
+// LEB128: https://en.wikipedia.org/wiki/LEB128
+func Write(w io.ByteWriter, v uint64) (n int, err error) {
+	more := true
+	for more {
+		c := uint8(v & 0x7f)
+		v >>= 7
+		more = v != 0
+		if more {
+			c |= 0x80
+		}
+		err = w.WriteByte(c)
+		if err != nil {
+			return n, err
+		}
+		n++
+	}
+	return n, nil
+}
