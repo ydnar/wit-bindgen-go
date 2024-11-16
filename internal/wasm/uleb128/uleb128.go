c3876bf96746a0b5ca757bab69dca1939de24114
Randy Reddig <randy.reddig@fastly.com>
2024-11-15 16:30:54 -0800

    internal/wasm/uleb128: add function Read

--- internal/wasm/uleb128/uleb128.go
+++ internal/wasm/uleb128/uleb128.go
@@ -3,6 +3,27 @@ package uleb128
 
 import "io"
 
+// Read reads an unsigned [LEB128] value from r.
+// Returns the value, number of bytes read, and/or an error.
+//
+// [LEB128]: https://en.wikipedia.org/wiki/LEB128
+func Read(r io.ByteReader) (v uint64, n int, err error) {
+	shift := 0
+	for {
+		b, err := r.ReadByte()
+		if err != nil {
+			return 0, n, err
+		}
+		n++
+		v |= uint64(b&0x7f) << shift
+		if (b & 0x80) == 0 {
+			break
+		}
+		shift += 7
+	}
+	return v, n, nil
+}
+
 // Write writes v in unsigned [LEB128] format to w.
 // Returns the number of bytes written and/or an error.
 //
