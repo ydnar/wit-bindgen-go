8353ee91e0a1934830ef272eb2f7d991003ebeb3
Randy Reddig <randy.reddig@fastly.com>
2024-11-25 20:24:18 -0800

    cmd/wit-bindgen-go/cmd/wit: use ConstrainTo

--- cmd/wit-bindgen-go/cmd/wit/wit.go
+++ cmd/wit-bindgen-go/cmd/wit/wit.go
@@ -3,13 +3,11 @@ package wit
 import (
 	"context"
 	"fmt"
-	"slices"
 
 	"github.com/urfave/cli/v3"
 
 	"go.bytecodealliance.org/internal/witcli"
 	"go.bytecodealliance.org/wit"
-	"go.bytecodealliance.org/wit/clone"
 )
 
 // Command is the CLI command for wit.
@@ -53,7 +51,7 @@ func action(ctx context.Context, cmd *cli.Command) error {
 		if i == nil {
 			return fmt.Errorf("interface %s not found", face)
 		}
-		res = filter(res, i)
+		res.ConstrainTo(i)
 	}
 
 	if world := cmd.String("world"); world != "" {
@@ -61,7 +59,7 @@ func action(ctx context.Context, cmd *cli.Command) error {
 		if w == nil {
 			return fmt.Errorf("world %s not found", world)
 		}
-		res = filter(res, w)
+		res.ConstrainTo(w)
 	}
 
 	fmt.Print(res.WIT(nil, ""))
@@ -84,54 +82,4 @@ func findInterface(r *wit.Resolve, pattern string) *wit.Interface {
 		}
 	}
 	return nil
-}
-
-func filter(res *wit.Resolve, node wit.Node) *wit.Resolve {
-	state := &clone.State{}
-	res = clone.Clone(state, res)
-	node = *clone.Clone(state, &node)
-
-	packages := slices.Clone(res.Packages)
-	res.Packages = nil
-	for _, pkg := range packages {
-		if !wit.DependsOn(node, pkg) && !wit.DependsOn(pkg, node) {
-			continue
-		}
-		res.Packages = append(res.Packages, pkg)
-
-		pkg.Worlds.All()(func(name string, w *wit.World) bool {
-			if !wit.DependsOn(w, node) {
-				pkg.Worlds.Delete(name)
-				return true
-			}
-
-			w.Imports.All()(func(name string, i wit.WorldItem) bool {
-				if !wit.DependsOn(node, i) {
-					w.Imports.Delete(name)
-				}
-				return true
-			})
-
-			w.Exports.All()(func(name string, i wit.WorldItem) bool {
-				if !wit.DependsOn(node, i) {
-					w.Exports.Delete(name)
-				}
-				return true
-			})
-
-			return true
-		})
-
-		pkg.Interfaces.All()(func(name string, i *wit.Interface) bool {
-			if !wit.DependsOn(node, i) {
-				pkg.Interfaces.Delete(name)
-			}
-			return true
-		})
-	}
-
-	// fmt.Printf("Resolve: %d %d %d %d\n\n",
-	// 	len(res.Worlds), len(res.Interfaces), len(res.TypeDefs), len(res.Packages))
-
-	return res
 }
