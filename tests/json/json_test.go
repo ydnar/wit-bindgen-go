718fa7c4f17538955ae6f5afeccf2f939e1bb561
Randy Reddig <randy.reddig@fastly.com>
2025-01-01 16:10:03 +1300

    tests/json: test json.Marshal and Unmarshal

--- tests/json/json_test.go
+++ tests/json/json_test.go
@@ -2,40 +2,76 @@ package json_test
 
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
 
 	wallclock "tests/generated/wasi/clocks/v0.2.0/wall-clock"
 	"tests/generated/wasi/filesystem/v0.2.0/types"
 )
 
-func TestRecordJSON(t *testing.T) {
-	var dt wallclock.DateTime
-	_ = dt
-}
-
-func TestEnumMarshalJSON(t *testing.T) {
+func TestJSON(t *testing.T) {
 	tests := []struct {
 		name    string
-		v       any
-		want    string
+		json    string
+		into    any
+		want    any
 		wantErr bool
 	}{
-		{"nil", nil, `null`, false},
-		{"descriptor-type(directory)", types.DescriptorTypeDirectory, `"directory"`, false},
+		{
+			"nil",
+			`null`,
+			ptr(ptr("")),
+			ptr((*string)(nil)),
+			false,
+		},
+		{
+			"descriptor-type(block-device)",
+			`"block-device"`,
+			ptr(types.DescriptorType(0)),
+			ptr(types.DescriptorTypeBlockDevice),
+			false,
+		},
+		{
+			"descriptor-type(directory)",
+			`"directory"`,
+			ptr(types.DescriptorType(0)),
+			ptr(types.DescriptorTypeDirectory),
+			false,
+		},
+		{
+			"datetime",
+			`{"seconds":1,"nanoseconds":2}`,
+			&wallclock.DateTime{},
+			&wallclock.DateTime{Seconds: 1, Nanoseconds: 2},
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := json.Marshal(tt.v)
+			err := json.Unmarshal([]byte(tt.json), &tt.into)
 			if tt.wantErr && err == nil {
-				t.Errorf("json.Marshal(%v): expected error, got nil error", tt.v)
+				t.Errorf("json.Unmarshal(%q): expected error, got nil error", tt.json)
 				return
 			} else if !tt.wantErr && err != nil {
-				t.Errorf("json.Marshal(%v): expected no error, got error: %v", tt.v, err)
+				t.Errorf("json.Unmarshal(%q): expected no error, got error: %v", tt.json, err)
 				return
 			}
-			if string(got) != tt.want {
-				t.Errorf("json.Marshal(%v): %s, expected %s", tt.v, string(got), tt.want)
+			if !reflect.DeepEqual(tt.want, tt.into) {
+				t.Errorf("json.Unmarshal(%q): resulting value different (%v != %v)", tt.json, tt.into, tt.want)
+				return
+			}
+			got, err := json.Marshal(tt.into)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			if string(got) != tt.json {
+				t.Errorf("json.Marshal(%v): %s, expected %s", tt.into, string(got), tt.json)
 			}
 		})
 	}
+}
+
+func ptr[T any](v T) *T {
+	return &v
 }
